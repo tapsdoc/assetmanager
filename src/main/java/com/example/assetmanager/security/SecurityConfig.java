bbea2887e88d4af8ae1c633a70fdcb2bdc18fd6e
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2023-04-13 12:23:26 +0200

    initial commit

--- src/main/java/com/example/assetmanager/security/SecurityConfig.java
+++ src/main/java/com/example/assetmanager/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/**").permitAll()
                 .anyRequest().permitAll()
             .and()
             .sessionManagement()
