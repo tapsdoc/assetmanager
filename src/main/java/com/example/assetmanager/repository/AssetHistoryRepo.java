30557d4fd0472c70a1ece0c8d9f9ac6e2bb21da7
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2023-04-13 11:56:41 +0200

    initial commit

--- src/main/java/com/example/assetmanager/repository/AssetHistoryRepo.java
+++ src/main/java/com/example/assetmanager/repository/AssetHistoryRepo.java
@@ -3,7 +3,6 @@ package com.example.assetmanager.repository;
 import com.example.assetmanager.domain.AssetHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
